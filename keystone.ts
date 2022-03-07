e91c7799ca9d4a25a50ad561f856fc035c63faea
Suzanne Rozier <suz@truss.works>
2022-03-07 13:22:45 -0600

    Remove session code, try fixing logs

--- keystone.ts
+++ keystone.ts
@@ -1,8 +1,9 @@
 import { config } from '@keystone-6/core'
 
 import { lists } from './src/schema'
-import { withSharedAuth } from './src/lib/auth'
+// import { withSharedAuth } from './src/lib/auth'
 
+<<<<<<< HEAD
 export default withSharedAuth(
   config({
     lists,
@@ -11,17 +12,25 @@ export default withSharedAuth(
       url: `${process.env.DATABASE_URL}` || '',
       enableLogging: true,
       useMigrations: true,
+=======
+export default config({
+  lists,
+  db: {
+    provider: 'postgresql',
+    url: `${process.env.DATABASE_URL}` || '',
+    enableLogging: true,
+    useMigrations: false, // TODO - set to true after deploying
+  },
+  ui: {
+    publicPages: ['/custom-page'],
+    // TODO - add redirect here if no session
+    // pageMiddleware: async ({ context, isValidSession }) => {},
+    isAccessAllowed: (context) => {
+      console.log('IMAGE TAG', process.env.IMAGE_TAG)
+      // console.log('check for session', context.session)
+      return true
+      // return !!context.session
+>>>>>>> Remove session code, try fixing logs
     },
-    ui: {
-      publicPages: ['/custom-page'],
-      // TODO - add redirect here if no session
-      // pageMiddleware: async ({ context, isValidSession }) => {},
-      isAccessAllowed: (context) => {
-        console.log('IMAGE TAG', process.env.IMAGE_TAG)
-        console.log('check for session', context.session)
-        return true
-        // return !!context.session
-      },
-    },
-  })
-)
+  },
+})
--- src/admin/pages/no-access.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NoAccess() {
-  return (
-    <>
-      <h1>Access not allowed</h1>
-    </>
-  )
-}
