32e2bbbce8ad820baf4d8107524cb0c30c0d7d87
Suzanne Rozier <suz@truss.works>
2022-03-11 11:09:34 -0600

    Redirect to portal login if no session

--- keystone.ts
+++ keystone.ts
@@ -1,7 +1,9 @@
+import url from 'url'
 import { config } from '@keystone-6/core'
 
 import { lists } from './src/schema'
 import { withSharedAuth } from './src/lib/auth'
+import { getAbsoluteUrl } from './src/util/getAbsoluteUrl'
 
 export default withSharedAuth(
   config({
@@ -14,11 +16,24 @@ export default withSharedAuth(
     },
     ui: {
       publicPages: ['/api/sysinfo'],
-      /*
       pageMiddleware: async ({ context, isValidSession }) => {
-      // TODO - add redirect to portal login here if no session
+        const { req, session } = context
+        const pathname = url.parse(req!.url!).pathname!
+
+        if (isValidSession) {
+          return
+        }
+
+        if (!session && pathname !== '/api/sysinfo') {
+          const requestUrl = getAbsoluteUrl(req).origin
+          const redirectUrl = `${requestUrl}${req!.url}`
+
+          const to = `${
+            process.env.PORTAL_URL
+          }/login?redirectTo=${encodeURIComponent(redirectUrl)}`
+          return { kind: 'redirect', to }
+        }
       },
-      */
       enableSessionItem: true,
       isAccessAllowed: ({ session }) => !!session,
     },
