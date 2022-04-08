2fdca9d4d125e816418abdc66caf2670d9180e00
Jacob Capps <jcapps@truss.works>
2022-04-08 16:30:46 -0500

    Add tests for Collection schema

--- src/schema.test.ts
+++ src/schema.test.ts
@@ -513,4 +513,181 @@ describe('Keystone schema', () => {
       })
     })
   })
+
+  describe('Collections', () => {
+    describe('as an admin user', () => {
+      const adminSession = {
+        name: 'Admin User',
+        userId: 'admin@example.com',
+        isAdmin: true,
+        isEnabled: true,
+      }
+
+      const testBookmark = {
+        url: 'http://www.example.com',
+        label: 'First Bookmark',
+        description: 'This is an example link',
+        keywords: 'example bookmark testing',
+      }
+
+      it('can create an empty collection', async () => {
+        const data = await context
+          .withSession(adminSession)
+          .query.Collection.createOne({
+            data: {
+              title: 'My Test Collection',
+            },
+            query: 'id title createdAt updatedAt',
+          })
+
+        expect(data).toMatchObject({
+          id: expect.any(String),
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          title: 'My Test Collection',
+        })
+
+        expect(data.createdAt).toBe(data.updatedAt)
+      })
+
+      it('can add a bookmark to a collection', async () => {
+        const bookmark = await context
+          .withSession(adminSession)
+          .query.Bookmark.createOne({
+            data: testBookmark,
+            query: 'id',
+          })
+        const collection = await context
+          .withSession(adminSession)
+          .query.Collection.createOne({
+            data: {
+              title: 'Test Collection',
+              bookmarks: { connect: { id: bookmark.id } },
+            },
+            query: 'id title bookmarks { id }',
+          })
+
+        expect(collection.id).toBeTruthy()
+        expect(collection.bookmarks).toHaveLength(1)
+        expect(collection.bookmarks[0].id).toEqual(bookmark.id)
+      })
+
+      it('can update a collection', async () => {
+        const existingCollection = await context
+          .withSession(adminSession)
+          .query.Collection.createOne({
+            data: {
+              title: 'My Test Collection',
+            },
+            query: 'id title createdAt updatedAt',
+          })
+
+        const data = await context
+          .withSession(adminSession)
+          .query.Collection.updateOne({
+            where: { id: existingCollection.id },
+            data: {
+              title: 'Updated Title',
+            },
+            query: 'id title createdAt updatedAt',
+          })
+
+        expect(data).toMatchObject({
+          id: expect.any(String),
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          title: 'Updated Title',
+        })
+      })
+
+      it('cannot delete a collection', async () => {
+        const existingCollections = await context
+          .withSession(adminSession)
+          .query.Collection.findMany({
+            query: 'id',
+          })
+
+        expect(
+          context.withSession(adminSession).query.Collection.deleteOne({
+            where: { id: existingCollections[0].id },
+          })
+        ).rejects.toThrow(
+          /Access denied: You cannot perform the 'delete' operation on the list 'Collection'./
+        )
+      })
+    })
+
+    describe('as a non admin user', () => {
+      const userSession = {
+        name: 'User 1',
+        userId: 'user1@example.com',
+        isAdmin: false,
+        isEnabled: true,
+      }
+
+      it('can query all Collections', async () => {
+        const data = await context
+          .withSession(userSession)
+          .query.Collection.findMany({
+            query: 'id title createdAt updatedAt',
+          })
+
+        expect(data[0]).toMatchObject({
+          id: expect.any(String),
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          title: 'My Test Collection',
+        })
+      })
+
+      it('cannot create collections', async () => {
+        expect(
+          context.withSession(userSession).query.Collection.createOne({
+            data: {
+              title: 'User Collection',
+            },
+            query: 'id title createdAt updatedAt',
+          })
+        ).rejects.toThrow(
+          /Access denied: You cannot perform the 'create' operation on the list 'Collection'./
+        )
+      })
+
+      it('cannot update a collection', async () => {
+        const existingCollections = await context
+          .withSession(userSession)
+          .query.Collection.findMany({
+            query: 'id',
+          })
+
+        expect(
+          context.withSession(userSession).query.Collection.updateOne({
+            where: { id: existingCollections[0].id },
+            data: {
+              title: 'Updating title by a non admin',
+            },
+            query: 'id title createdAt updatedAt',
+          })
+        ).rejects.toThrow(
+          /Access denied: You cannot perform the 'update' operation on the list 'Collection'./
+        )
+      })
+
+      it('cannot delete collections', async () => {
+        const existingCollections = await context
+          .withSession(userSession)
+          .query.Collection.findMany({
+            query: 'id',
+          })
+
+        expect(
+          context.withSession(userSession).query.Collection.deleteOne({
+            where: { id: existingCollections[0].id },
+          })
+        ).rejects.toThrow(
+          /Access denied: You cannot perform the 'delete' operation on the list 'Collection'./
+        )
+      })
+    })
+  })
 })
