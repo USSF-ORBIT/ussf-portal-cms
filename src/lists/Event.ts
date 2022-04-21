7879f239b713ffa5875b2861e5056bdb36f0770f
Suzanne Rozier <suz@truss.works>
2022-04-21 14:53:26 -0500

    Add Event list

---
+++ src/lists/Event.ts
@@ -0,0 +1,44 @@
+import { list } from '@keystone-6/core'
+import { json, relationship, text } from '@keystone-6/core/fields'
+
+import type { Lists } from '.keystone/types'
+
+import { isAdmin } from '../util/access'
+import { withAtTracking } from '../util/tracking'
+
+const Event: Lists.Event = list(
+  withAtTracking({
+    access: {
+      operation: {
+        create: () => false,
+        read: isAdmin,
+        update: () => false,
+        delete: () => false,
+      },
+    },
+
+    ui: {
+      description: 'A generated log of all CMS mutations',
+      isHidden: !isAdmin,
+      hideCreate: true,
+      hideDelete: true,
+      itemView: {
+        defaultFieldMode: 'read',
+      },
+    },
+
+    fields: {
+      operation: text(), // create, update, or delete
+      itemListKey: text(), // the list being operated on
+      itemId: text(), // the ID of the item being operated on
+      inputData: json(), // the user input data
+      resolvedData: json(), // the resolved data
+      changedData: json(), // for update only, a diff
+      originalItem: json(), // the item before changes
+      item: json(), // the item after changes
+      actor: relationship({ ref: 'User' }), // the user who performed the operation
+    },
+  })
+)
+
+export default Event
