3c89275f1db32e1d83d7779e2a48d2e965f42ae9
Suzanne Rozier <suz@truss.works>
2022-04-27 14:27:33 -0500

    [WIP] Add event logging (#39)
    * Add logging via plugin
    
    * Consolidate tracking fns
    
    * Add Event list

--- src/lists/Collection.ts
+++ src/lists/Collection.ts
@@ -4,42 +4,40 @@ import { checkbox, relationship, text } from '@keystone-6/core/fields'
 import type { Lists } from '.keystone/types'
 
 import { isAdmin, editReadAdminUI } from '../util/access'
-import { withAtTracking, withByTracking } from '../util/tracking'
+import { withAllTracking } from '../util/tracking'
 
 const Collection: Lists.Collection = list(
-  withByTracking(
-    withAtTracking({
-      access: {
-        operation: {
-          create: isAdmin,
-          query: () => true,
-          update: isAdmin,
-          delete: () => false,
-        },
+  withAllTracking({
+    access: {
+      operation: {
+        create: isAdmin,
+        query: () => true,
+        update: isAdmin,
+        delete: () => false,
       },
+    },
 
-      ui: {
-        hideCreate: ({ session }) => !isAdmin({ session }),
-        hideDelete: true,
-        itemView: {
-          defaultFieldMode: editReadAdminUI,
-        },
+    ui: {
+      hideCreate: ({ session }) => !isAdmin({ session }),
+      hideDelete: true,
+      itemView: {
+        defaultFieldMode: editReadAdminUI,
       },
+    },
 
-      fields: {
-        title: text({
-          validation: {
-            isRequired: true,
-          },
-        }),
-        bookmarks: relationship({ ref: 'Bookmark.collections', many: true }),
-        showInSitesApps: checkbox({
-          defaultValue: false,
-          label: 'Show in Sites & Apps',
-        }),
-      },
-    })
-  )
+    fields: {
+      title: text({
+        validation: {
+          isRequired: true,
+        },
+      }),
+      bookmarks: relationship({ ref: 'Bookmark.collections', many: true }),
+      showInSitesApps: checkbox({
+        defaultValue: false,
+        label: 'Show in Sites & Apps',
+      }),
+    },
+  })
 )
 
 export default Collection
