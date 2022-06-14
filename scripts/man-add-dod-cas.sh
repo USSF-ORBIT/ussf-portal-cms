6e939f1acb538ba1079362fcfffdd5942363ed2e
Joshua Sarnovsky <jsarnovsky@truss.works>
2022-06-13 21:25:01 -0400

    add *

--- scripts/man-add-dod-cas.sh
+++ scripts/man-add-dod-cas.sh
@@ -21,7 +21,7 @@
                awk -F '(=|= )' '{gsub(/ /, "_", $NF); print $NF}'
         )
         mv $i ${name}.crt
-        cp /etc/docker/certs.d/
+        cp /etc/docker/certs.d/*
     done
 
     # update certificate stores
