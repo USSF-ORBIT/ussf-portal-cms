87e58fbe676b1cbc40449515be381af512cd796a
Abigail Young <abbyoung@gmail.com>
2022-11-15 11:38:03 -0800

    remove path resolve

--- src/lists/Announcement.ts
+++ src/lists/Announcement.ts
@@ -1,7 +1,6 @@
 import { list } from '@keystone-6/core'
 import { select, text, timestamp } from '@keystone-6/core/fields'
 import { document } from '@keystone-6/fields-document'
-import path from 'path'
 import { ANNOUNCEMENT_STATUSES } from '../util/workflows'
 import {
   canCreateArticle,
@@ -60,7 +59,7 @@ const Announcement = list(
         },
         links: true,
         ui: {
-          views: path.resolve('./src/components/component-blocks'),
+          views: './src/components/component-blocks',
         },
         componentBlocks,
       }),
