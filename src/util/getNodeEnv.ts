abc2d671e7dc159670a4ccebaa67f0f5a92d1c8e
Abigail Young <abbyoung@gmail.com>
2022-12-08 15:02:20 -0800

    check only ecs env var

--- src/util/getNodeEnv.ts
+++ src/util/getNodeEnv.ts
@@ -1,6 +1,3 @@
 export const isProduction = (): boolean => {
-  return (
-    process.env.NODE_ENV === 'production' &&
-    process.env.USE_S3_STORAGE === 'true'
-  )
+  return process.env.USE_S3_STORAGE === 'true'
 }
