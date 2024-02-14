1df1f918d7fba907c4aec9164aa3640807fe7a51
Jacob Capps <jcapps@truss.works>
2024-02-14 15:35:03 -0600

    Add access and limit characters

--- src/lists/SiteHeader.ts
+++ src/lists/SiteHeader.ts
@@ -1,12 +1,13 @@
 import { list } from '@keystone-6/core'
 import { text } from '@keystone-6/core/fields'
+import { canCreateSiteHeader, canUpdateSiteHeader } from '../util/access'
 
 const SiteHeader = list({
   access: {
     operation: {
-      create: () => true,
+      create: canCreateSiteHeader,
       query: () => true,
-      update: () => true,
+      update: canUpdateSiteHeader,
       delete: () => false,
     },
   },
@@ -14,16 +15,19 @@ const SiteHeader = list({
   isSingleton: true,
 
   ui: {
-    hideCreate: false,
+    hideCreate: ({ session }) => !canCreateSiteHeader({ session }),
     hideDelete: true,
     itemView: {
-      defaultFieldMode: 'read',
+      defaultFieldMode: 'edit',
     },
   },
   fields: {
     buttonLabel: text({
       validation: {
         isRequired: true,
+        length: {
+          max: 20,
+        },
       },
       label: 'Button label',
     }),
@@ -32,20 +36,25 @@ const SiteHeader = list({
         isRequired: true,
       },
       ui: {
-        description:
-          'The source must be a relative (/) or absolute (https://) URL from the portal.',
+        description: 'The source must be a relative (/) URL from the portal.',
       },
       label: 'Button source',
     }),
     dropdownLabel: text({
       validation: {
         isRequired: true,
+        length: {
+          max: 20,
+        },
       },
       label: 'Dropdown label',
     }),
     dropdownItem1Label: text({
       validation: {
         isRequired: true,
+        length: {
+          max: 20,
+        },
       },
       label: 'Dropdown item 1 label',
     }),
@@ -55,38 +64,49 @@ const SiteHeader = list({
       },
       label: 'Dropdown item 1 source',
       ui: {
-        description:
-          'The source must be a relative (/) or absolute (https://) URL from the portal.',
+        description: 'The source must be a relative (/) URL from the portal.',
       },
     }),
     dropdownItem2Label: text({
       label: 'Dropdown item 2 label',
+      validation: {
+        length: {
+          max: 20,
+        },
+      },
     }),
     dropdownItem2Source: text({
       label: 'Dropdown item 2 source',
       ui: {
-        description:
-          'The source must be a relative (/) or absolute (https://) URL from the portal.',
+        description: 'The source must be a relative (/) URL from the portal.',
       },
     }),
     dropdownItem3Label: text({
       label: 'Dropdown item 3 label',
+      validation: {
+        length: {
+          max: 20,
+        },
+      },
     }),
     dropdownItem3Source: text({
       label: 'Dropdown item 3 source',
       ui: {
-        description:
-          'The source must be a relative (/) or absolute (https://) URL from the portal.',
+        description: 'The source must be a relative (/) URL from the portal.',
       },
     }),
     dropdownItem4Label: text({
       label: 'Dropdown item 4 label',
+      validation: {
+        length: {
+          max: 20,
+        },
+      },
     }),
     dropdownItem4Source: text({
       label: 'Dropdown item 4 source',
       ui: {
-        description:
-          'The source must be a relative (/) or absolute (https://) URL from the portal.',
+        description: 'The source must be a relative (/) URL from the portal.',
       },
     }),
   },
